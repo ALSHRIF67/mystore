b582c93d141f382f5e17f573d1a263147ccb45a2
Alshrif Omer <mr048530@gmail.com>
2025-11-22 02:35:56 -0800

    Add project sorting, pagination UI; add creator/updater relationships

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class = "dark">
 <head>
         {{-- Theme initialization script: applies dark class early to avoid FOUC --}}
         <script>
